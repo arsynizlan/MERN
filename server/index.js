e268050e9b15d29c838525753375158376df0c05
Arsy Nizlan Ramadhan <arsynizlanr@gmail.com>
2022-12-24 23:46:04 +0700

    configure mongodb database connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+
+// Database Connection
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Error => ", err));
 
 app.get("/", (req, res) => {
   res.json({
