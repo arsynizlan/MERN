9e06ab1806ee36d8f4ebae2812a7fb656bd6a49c
Arsy Nizlan Ramadhan <arsynizlanr@gmail.com>
2022-12-24 22:58:57 +0700

    Hello World!

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    data: "Hello World",
+  });
+});
+
+app.listen(8000, () => {
+  console.log("listening on http://localhost:8000");
+});
